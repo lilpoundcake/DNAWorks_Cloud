28ba90085022807f81d9e2983ea9cefdf8c24218
lilpoundcake <44047612+lilpoundcake@users.noreply.github.com>
2021-11-11 22:19:46 +0700

    Update Logfile_generate.py

--- Logfile_generate.py
+++ Logfile_generate.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+log_title = ""
+
 project_name = input("Введите название проекта\n") """Название проекта и имя входных файлов"""
 react_temp = input("Введите температуру проведения реакции\n")
 primer_lenth = input("Введите желаемую длину праймеров\n")
@@ -20,6 +22,38 @@ else:
   seq_nt = input("Введите нуклеотидную последовательность\n")
   seq_prot = 0
 
-if len(seq) >= 500 and seq_prot == 0:
-  fragment_num = round(len(seq)/350)
-  fragment_len = round(seq_nt/)
+fragment_list = []
+if seq_prot == 0:
+  seq_len = len(seq_nt)
+else:
+  seq_len = len(seq_prot)                 
+                   
+if len(seq_nt) >= 500 and seq_prot == 0:
+  fragment_num = round(len(seq_nt)/350)
+  fragment_len = round(seq_nt/fragment_num)
+  for i in range(fragment_num):
+    if i == fragment_num-1:
+      fragment_list.append(seq_nt[seq_len-fragment_len*i)):len(seq_nt)])
+    else:
+      fragment_list.append(seq_nt[fragment_len*i:fragment_len*(i+1)])
+elif len(seq_nt) < 500:
+  fragment_num = 1
+  fragment_list.append(seq_nt)
+elif len(seq_prot) >= 166 and seq_nt == 0:
+  fragment_num = round(len(seq_prot)/166)
+  fragment_len = round(seq_prot/fragment_num)
+  for i in range(fragment_num):
+    if i == fragment_num-1:
+      fragment_list.append(seq_prot[seq_len-fragment_len*i)):len(seq_prot)])
+    else:
+      fragment_list.append(seq_prot[fragment_len*i:fragment_len*(i+1)])
+else:
+  fragment_num = 1
+  fragment_list.append(seq_prot)
+                   
+
+                   
+                   
+                   
+                   
+                   
