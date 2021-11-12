7488c1b62a40e04c228aee1840d68efcdb265685
lilpoundcake <44047612+lilpoundcake@users.noreply.github.com>
2021-11-12 11:13:44 +0700

    Update Logfile_generate.py

--- Logfile_generate.py
+++ Logfile_generate.py
@@ -43,20 +43,20 @@ if seq_prot == "":
 else:
   seq_len = len(seq_prot)                 
                    
-if len(seq_len) >= 500 and seq_prot == "":
+if seq_len >= 500 and seq_prot == "":
   fragment_num = round(len(seq_nt)/350)
-  fragment_len = round(seq_nt/fragment_num)
+  fragment_len = round(seq_len/fragment_num)
   for i in range(fragment_num):
     if i == fragment_num-1:
       fragment_list.append(seq_nt[(seq_len-fragment_len*i):len(seq_nt)])
     else:
       fragment_list.append(seq_nt[(fragment_len*i):(fragment_len*(i+1))])
-elif len(seq_len) < 500:
+elif seq_len < 500:
   fragment_num = 1
   fragment_list.append(seq_nt)
-elif len(seq_len) >= 166 and seq_nt == "":
+elif seq_len >= 166 and seq_nt == "":
   fragment_num = round(len(seq_prot)/166)
-  fragment_len = round(seq_prot/fragment_num)
+  fragment_len = round(seq_len/fragment_num)
   for i in range(fragment_num):
     if i == fragment_num-1:
       fragment_list.append(seq_prot[(seq_len-fragment_len*i):len(seq_prot)])
