ad65ca2b463207a28f1ee4c290f75563e90e5f91
lilpoundcake <44047612+lilpoundcake@users.noreply.github.com>
2021-11-12 13:36:38 +0700

    Update Logfile_generate.py

--- Logfile_generate.py
+++ Logfile_generate.py
@@ -94,7 +94,7 @@ bash_script.write("mkdir " + project_name + "\n" )
 for i in range(fragment_num):
   bash_script.write("cp " + project_name + "_" + str(i+1) + ".inp " + str(project_name) + "\n")
 
-bash_script.write("cp " + project_name + ".txt " + str(project_name) + "\n")
+#bash_script.write("cp " + project_name + ".txt " + str(project_name) + "\n")
 bash_script.write("cp " + project_name + "_hairpin.txt " + str(project_name) + "\n")
 bash_script.write("cp " + project_name + "_high_temp.txt " + str(project_name) + "\n")
 
